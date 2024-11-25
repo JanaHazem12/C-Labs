c938a51a01c39aa0dcb239efd24b845c19f32bec
JanaHazem12 <jana.hazem.abbass@gmail.com>
2024-11-25 22:45:35 +0200

    Lab6 Submission

--- LAB5/BuiltInFunctions/BuiltInFunc.h
+++
@@ -1,6 +0,0 @@
-int strLength_jana(char str[]); // DONEEEE
-void strCopy_jana(char source[], char dest[]); // DONEEEE
-void lowerToUpper_jana(char strLU[]); // DONEEEE
-void upperTolower_jana(char strUL[]); // DONEEEE
-void strConcat_jana(char dest[], char source[]); // DONEEEE
-int strComp_jana(char dest[], char source[]);// DONEEEE
